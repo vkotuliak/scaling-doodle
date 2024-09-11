eea82d88bc4806f8cb6808d1b4ebac36e80db7e5
viktorkotuliak <viktor.kotuliak@gmail.com>
2024-09-11 15:29:20 +0200

    fixed main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import numpy as np
 
 list_of_numbers = []
-for i in range(1, 10):
+for i in range(1, 11):
     list_of_numbers.append(i)
 
 array_of_numbers = np.array(list_of_numbers)
 
 print(array_of_numbers)
-    
